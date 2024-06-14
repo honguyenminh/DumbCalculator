2119ad634c9b5b46296b94aee78990f39727568d
Nguyên Minh Hồ <honguyenminh@outlook.com>
2024-06-14 10:22:52 +0700

    Add basic number input functionality

--- DumbCalculator/MainWindow.xaml.cs
+++ DumbCalculator/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -24,15 +25,61 @@ namespace DumbCalculator
     /// </summary>
     public sealed partial class MainWindow : WinUIEx.WindowEx
     {
+        public MainWindowViewModel ViewModel { get; set; } = new();
+
         public MainWindow()
         {
-            this.InitializeComponent();
             ExtendsContentIntoTitleBar = true;
+            this.InitializeComponent();
+            foreach (var childElement in ButtonGrid.Children)
+            {
+                if (childElement is not Button) continue;
+                var button = (Button)childElement;
+                if (button.Content is not string) continue;
+                bool isNumeric = IsNumericRegex().IsMatch((string)button.Content);
+                if (isNumeric)
+                    button.Click += NumberButton_Clicked;
+            }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NumberButton_Clicked(object sender, RoutedEventArgs e)
         {
-            test.IsEnabled = true;
+            Button button = (sender as Button)!;
+            string number = (button.Content as string)!;
+            ViewModel.Input += number;
         }
+
+        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewModel.Input.Length == 0) return;
+            if (ViewModel.Input[^1] == '.') _decimalPointExists = false;
+            ViewModel.Input = ViewModel.Input[..^1];
+        }
+
+        private void ClearEnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            _decimalPointExists = false;
+            ViewModel.Input = string.Empty;
+        }
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearEnterButton_Click(sender, e);
+            ViewModel.PendingOperation = string.Empty;
+            throw new NotImplementedException("Them xoa ops di tml");
+        }
+
+        private bool _decimalPointExists = false;
+        private void DecimalButton_Click(object? sender, RoutedEventArgs? e)
+        {
+            if (_decimalPointExists) return;
+            _decimalPointExists = true;
+            if (ViewModel.Input.Length == 0) 
+                ViewModel.Input = "0";
+
+            ViewModel.Input += '.';
+        }
+
+        [GeneratedRegex(@"^\d$")]
+        private static partial Regex IsNumericRegex();
     }
 }
---
+++ DumbCalculator/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DumbCalculator
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _input = string.Empty;
+        public string Input { get => _input; set { _input = value; OnPropertyChanged(); } }
+
+        // todo: switch to operation class and convert from that
+        private string _pendingOperation = string.Empty;
+        public string PendingOperation { get => _pendingOperation; set { _pendingOperation = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
